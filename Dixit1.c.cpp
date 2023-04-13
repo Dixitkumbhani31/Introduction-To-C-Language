984a02f048e31d0d48f2916cb28c7d38edc78616
Dixitkumbhani31 <130464783+Dixitkumbhani31@users.noreply.github.com>
2023-04-13 22:02:49 +0530

    Add files via upload

---
+++ Dixit1.c.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+main()
+{
+	printf("Hello...");
+	printf("my name is dixit");
+	printf("my father name is babubhai");
+	printf("my age is 17 years");
+}
---
+++ Dixit3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf(" -------\n");
+	printf("|\t|\n");
+	printf("D\t|\n");
+	printf("I\t|\n");
+    printf("X\t|\n");
+	printf("I\t|\n");
+	printf("T\t|\n");
+	printf("|\t|\n");
+	printf(" -------\n");	
+}
